984a14fe4b3f19c9b4f04ebc11c26cd48b6d635e
Valeria Yanycheva <valeria.yanycheva@gmail.com>
2022-07-03 14:01:59 +0600

    feat: add dimming the screen when the menu is active

--- travel/script.js
+++ travel/script.js
@@ -2,18 +2,22 @@ const burger = document.querySelector('.header__burger');
 const burgerSpan = document.querySelectorAll('.header__burger');
 const menu = document.querySelector('.header__navbar');
 const navbarClose = document.querySelector('.navbar__x');
+const shadow = document.querySelector('.wrapper__shadow');
 
 burger.addEventListener('click', function() {
     menu.classList.add('_active');
+    shadow.classList.add('_active');
 });
 
 navbarClose.addEventListener('click', function() {
     menu.classList.remove('_active');
+    shadow.classList.remove('_active');
 })
 
 document.addEventListener('click', function(event) {
     if(event.target != menu && event.target.parentElement != menu && event.target != burger && event.target.parentElement != burger && menu.classList.contains('_active')) {
         menu.classList.remove('_active');
+        shadow.classList.remove('_active');
         console.log(event.target);
     }
 })
